22b1723768ce4075557947560c137fa112d3c97a
predrag jandric <predrag.jandric.bg@gmail.com>
2024-07-07 23:51:11 +0200

    ADDED: full logic for adding items to the packing list. all components still live in App.js file. in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import initialItems from "../src/exampleData"
 
 export default function App() {
+  const [items, setItems] = useState([]);
 
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
   return (
     <div className="App">
       <Logo />
-
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,14 +23,56 @@ function Logo() {
 }
 
 // **************** FORM
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    console.log(e);
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        type="text"
+        placeholder="item...."
+        value={description}
+      />
+      <button>add</button>
+    </form>
+  );
+}
 
 // **************** PACKING LIST
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
